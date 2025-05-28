b96af1eda86fa9dcb14a3ec7b46c67130ecd4f0c
Andrew Drake <andrew@below43.com>
2025-05-28 19:46:00 +1200

    Add title and hide params

--- wp-simple-noticeboard.php
+++ wp-simple-noticeboard.php
@@ -74,6 +74,9 @@ function display_notice_meta_box($notice)
 	$date_from = get_post_meta($notice->ID, 'date_from', true);
 	$date_to = get_post_meta($notice->ID, 'date_to', true);
 	$notice_text = get_post_meta($notice->ID, 'notice_text', true);
+	$date_enabled = get_post_meta($notice->ID, 'date_enabled', true); // New meta
+
+	if ($date_enabled === '') $date_enabled = '1'; // Default to enabled for existing posts
 ?>
 	<table>
 		<tr>
@@ -85,16 +88,41 @@ function display_notice_meta_box($notice)
 			<td><input type="text" size="80" name="notice_url" id="notice_url" value="<?php echo $url; ?>" /></td>
 		</tr>
 		<tr>
-			<td style="width: 100%">Notice Display Date From</td>
-			<td><input type="date" name="notice_date_from" id="notice_date_from" value="<?php echo $date_from; ?>" /></td>
+			<td colspan="2">
+				<label>
+					<input type="checkbox" name="notice_date_enabled" id="notice_date_enabled" value="1" <?php checked($date_enabled, '1'); ?> />
+					Enable date range (show/hide notice based on date)
+				</label>
+			</td>
 		</tr>
-		<tr>
+		<tr class="notice-date-row">
+			<td style="width: 100%">Notice Display Date From</td>
+			<td><input type="date" name="notice_date_from" id="notice_date_from" value="<?php echo esc_attr($date_from); ?>" /></td>
+		</tr>
+		<tr class="notice-date-row">
 			<td style="width: 100%">Notice Display Date To</td>
-			<td><input type="date" name="notice_date_to" id="notice_date_to" value="<?php echo $date_to; ?>" /></td>
+			<td><input type="date" name="notice_date_to" id="notice_date_to" value="<?php echo esc_attr($date_to); ?>" /></td>
 		</tr>
 	</table>
+	<script>
+		jQuery(function($) {
+			function toggleDateRows() {
+				if ($('#notice_date_enabled').is(':checked')) {
+					$('.notice-date-row').show();
+				} else {
+					$('.notice-date-row').hide();
+					// Clear the date fields when disabling
+					$('#notice_date_from').val('');
+					$('#notice_date_to').val('');
+				}
+			}
+			$('#notice_date_enabled').on('change', toggleDateRows);
+			toggleDateRows();
+		});
+	</script>
 	<?php
 }
+
 
 function save_notice_meta_fields($post_id)
 {
@@ -103,18 +131,11 @@ function save_notice_meta_fields($post_id)
 	}
 
 	if (get_post_type($post_id) == 'notice') {
-		if (isset($_POST['notice_text'])) {
-			update_post_meta($post_id, 'notice_text', $_POST['notice_text']);
-		}
-		if (isset($_POST['notice_url'])) {
-			update_post_meta($post_id, 'url', $_POST['notice_url']);
-		}
-		if (isset($_POST['notice_date_from'])) {
-			update_post_meta($post_id, 'date_from', $_POST['notice_date_from']);
-		}
-		if (isset($_POST['notice_date_to'])) {
-			update_post_meta($post_id, 'date_to', $_POST['notice_date_to']);
-		}
+		update_post_meta($post_id, 'notice_text', sanitize_text_field($_POST['notice_text'] ?? ''));
+		update_post_meta($post_id, 'url', sanitize_text_field($_POST['notice_url'] ?? ''));
+		update_post_meta($post_id, 'date_from', sanitize_text_field($_POST['notice_date_from'] ?? ''));
+		update_post_meta($post_id, 'date_to', sanitize_text_field($_POST['notice_date_to'] ?? ''));
+		update_post_meta($post_id, 'date_enabled', isset($_POST['notice_date_enabled']) ? '1' : '0');
 	}
 }
 
@@ -140,15 +161,17 @@ function notice_admin_script()
 						$('#notice_text').focus();
 						return false;
 					}
-					if ($('#notice_date_to').val() == '') {
-						alert('Date To is required');
-						$('#notice_date_to').focus();
-						return false;
-					}
-					if ($('#notice_date_from').val() == '') {
-						alert('Date From is required');
-						$('#notice_date_from').focus();
-						return false;
+					if ($('#notice_date_enabled').is(':checked')) {
+						if ($('#notice_date_to').val() == '') {
+							alert('Date To is required');
+							$('#notice_date_to').focus();
+							return false;
+						}
+						if ($('#notice_date_from').val() == '') {
+							alert('Date From is required');
+							$('#notice_date_from').focus();
+							return false;
+						}
 					}
 				});
 			});
@@ -168,21 +191,21 @@ function display_noticeboard_func($atts)
 	$date_from = get_post_meta($notice_id, 'date_from', true);
 	$date_to = get_post_meta($notice_id, 'date_to', true);
 	$notice_text = get_post_meta($notice_id, 'notice_text', true);
-
+	$date_enabled = get_post_meta($notice_id, 'date_enabled', true);
 
 	//get the 200 wide, x high version of the thumbnail
 	$thumbnail = get_the_post_thumbnail($notice_id, array(200, 200));
 
-	// Convert the dates to Unix timestamps
-	$date_from_time = strtotime($date_from);
-	$date_to_time = strtotime($date_to);
-	$current_date = date('Y-m-d');
-	$current_date_time = strtotime($current_date);
+	// Only check date if enabled
+	if ($date_enabled === '1') {
+		$date_from_time = strtotime($date_from);
+		$date_to_time = strtotime($date_to);
+		$current_date = date('Y-m-d');
+		$current_date_time = strtotime($current_date);
 
-	// Skip the post if the current date is not within the date range
-	if ($current_date_time < $date_from_time || $current_date_time > $date_to_time) {
-		return "<p>This notice has expired.</p>";
-		exit;
+		if ($current_date_time < $date_from_time || $current_date_time > $date_to_time) {
+			return "<p>This notice has expired.</p>";
+		}
 	}
 
 	$output = "<h2>{$notice->post_title}</h2>
@@ -204,6 +227,8 @@ function display_noticeboard_list_func($atts)
 	$atts = shortcode_atts(
 		array(
 			'category' => '', // Default value
+            'title' => '',    // Optional title
+            'hide_if_empty' => '0',
 		),
 		$atts,
 		'display_noticeboard_list_func'
@@ -241,34 +266,36 @@ function display_noticeboard_list_func($atts)
 	);
 
 	$notices = new WP_Query($args);
-	$output = '<br/><br/><ul class="simple-noticeboard-list">';
-	$count = 0;
+
+    $output = '<br/><br/>';
+    if (!empty($atts['title'])) {
+        $output .= '<h2>' . esc_html($atts['title']) . '</h2>';
+    }
+    $output .= '<ul class="simple-noticeboard-list">';
+    $count = 0;
 	if ($notices->have_posts()) {
 		while ($notices->have_posts()) {
 			$notices->the_post();
 			$notice_id = get_the_ID();
 			$notice_text = get_post_meta($notice_id, 'notice_text', true);
 			$notice_text = wp_trim_words($notice_text, 40);
-			
-			$thumbnail = get_the_post_thumbnail($notice_id, array(300, 225)); 
+
+			$thumbnail = get_the_post_thumbnail($notice_id, array(300, 225));
 			$categories = get_the_category_list(', ');
 
-			$date_from = get_post_meta($notice_id, 'date_from', true);
-			$date_to = get_post_meta($notice_id, 'date_to', true);
-			$current_date = date('Y-m-d');
-
-			// Convert the dates to Unix timestamps
-			$date_from = strtotime($date_from);
-			$date_to = strtotime($date_to);
-			$current_date = strtotime($current_date);
-
 			// Skip the post if the current date is not within the date range
-			if ($current_date < $date_from || $current_date > $date_to) {
-				continue;
+			$date_enabled = get_post_meta($notice_id, 'date_enabled', true);
+			if ($date_enabled === '1') {
+				$date_from = strtotime(get_post_meta($notice_id, 'date_from', true));
+				$date_to = strtotime(get_post_meta($notice_id, 'date_to', true));
+				$current_date = strtotime(date('Y-m-d'));
+				if ($current_date < $date_from || $current_date > $date_to) {
+					continue;
+				}
 			}
 
-			$title= "<a style=\"text-decoration:none\" href='" . get_permalink() . "'>" . get_the_title() . "</a>";
-	
+			$title = "<a style=\"text-decoration:none\" href='" . get_permalink() . "'>" . get_the_title() . "</a>";
+
 			$output .= "<li>
 							<center>{$thumbnail}</center>
 							<h2>" . $title . "</h2>
@@ -276,14 +303,17 @@ function display_noticeboard_list_func($atts)
 			//view more link
 			$output .= "<p class=\"view-more-link\"><a href='" . get_permalink() . "'>View more</a></p>";
 			$output .= "</li>";
-		
+
 			$count++;
-			
 		}
 	}
-	if (!$count) {
-		$output .= '<li>There are currently no notices to be displayed.</li>';
-	}
+    if (!$count) {
+        if ($atts['hide_if_empty'] === '1') {
+            return ''; // Hide all output if empty and hide_if_empty is set
+        } else {
+            $output .= '<li>There are currently no notices to be displayed.</li>';
+        }
+    }
 	wp_reset_postdata();
 	$output .= '</ul>';
 	return $output;
